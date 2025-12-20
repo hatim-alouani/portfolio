8d8b76bd32daea242a8cf76442561bdd62dcc808
haalouan <haalouan@student.1337.ma>
2025-12-20 18:22:41 +0100

    push

--- app/page.tsx
+++ app/page.tsx
@@ -312,8 +312,8 @@ export default function Portfolio() {
                 className="text-lg px-8 bg-transparent hover:scale-105 transition-all duration-300 border-primary/30 hover:border-primary hover:bg-primary/10"
                 onClick={() => {
                   const link = document.createElement("a")
-                  link.href = "/Hatim_Alouani_CV.pdf"
-                  link.download = "Hatim_Alouani_CV.pdf"
+                  link.href = "/hatim_alouani.pdf"
+                  link.download = "hatim_alouani.pdf"
                   document.body.appendChild(link)
                   link.click()
                   document.body.removeChild(link)
